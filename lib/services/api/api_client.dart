9bc90d5a734a4fa3d24ea5983ce6f222c66a9262
YHP125 <yohanesbrahmantya@gmail.com>
2023-11-30 21:49:48 +0700

    API_CW

--- lib/services/api/api_client.dart
+++ lib/services/api/api_client.dart
@@ -1,5 +1,5 @@
 class ApiClient {
-  final String domainName = '10.53.3.131';
+  final String domainName = '172.16.59.35';
   late final String baseUrl;
   ApiClient() {
     baseUrl = 'http://$domainName:8000/api';
