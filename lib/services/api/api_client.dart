65e762766e18bb7009c7b0da63a6946cd9e24702
DanielNC123 <210711346@students.uajy.ac.id>
2023-11-30 19:36:36 +0700

    ganti api link

--- lib/services/api/api_client.dart
+++ lib/services/api/api_client.dart
@@ -1,7 +1,7 @@
 class ApiClient {
-  final String domainName = '10.53.9.58';
+  final String domainName = '172.16.59.35';
   late final String baseUrl;
   ApiClient() {
-    baseUrl = 'http://$domainName:7000/api';
+    baseUrl = 'http://$domainName:8000/api';
   }
 }
